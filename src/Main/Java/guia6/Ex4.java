f96033920685671c44f6cfce50c7ca25f86c30ab
nicocurat <nicurat@gmail.com>
2017-05-23 00:06:26 -0300

    Finished exercise 4.

--- src/Main/Java/guia6/Ex4.java
+++ src/Main/Java/guia6/Ex4.java
@@ -7,6 +7,29 @@ public class Ex4 implements Exercise4 {
 
     @Override
     public double[][] summation(double[] matrixA, double[] matrixB, Calculator calculator) {
-        return new double[0][];
+        if(matrixA.length == matrixB.length){
+            double[] vector = new double[matrixA.length];
+            for (int i = 0; i < matrixA.length; i++) {
+                vector[i] = calculator.sum(matrixA[i], matrixB[i]);
+            }
+            int resultLength = lengthOfMatrix(vector.length, calculator);
+            double[][] result = new double[resultLength][resultLength];
+            int aux = 0;
+            for (int i = 0; i < resultLength; i++) {
+                for (int j = 0; j < resultLength; j++) {
+                    if(j < i) result[i][j] = 0;
+                    else {
+                        result[i][j] = vector[aux];
+                        aux++;
+                    }
+                }
+            }
+            return result;
+        }
+        throw new RuntimeException("Both vectors must have the same length");
+    }
+
+    private int lengthOfMatrix(int arrayLength, Calculator calculator){
+        return (int) calculator.division(calculator.sum(-1, Math.sqrt(calculator.sum(1, calculator.multiplication(8, arrayLength)))), 2);
     }
 }
